f5f3d158aa74ec0a42afe404fc3b1737639c4cf4
guxta77 <gustavoveroni10@gmail.com>
2025-05-24 18:24:19 -0300

    criando projeto e sistema de autenticação

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBl1ySrW-qKNSH_f3zLv4ey-rOnNtHhW30",
+    authDomain: "projectguxta.firebaseapp.com",
+    projectId: "projectguxta",
+    storageBucket: "projectguxta.appspot.com",
+    messagingSenderId: "513942433834",
+    appId: "1:513942433834:web:7f9d081e1d3ba269c4d353",
+  }
+};
